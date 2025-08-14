568d2e9d64011b06f2e89e550277e5f5d9112f28
dbaturac <doruk@appcircle.io>
2025-08-14 11:10:31 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
